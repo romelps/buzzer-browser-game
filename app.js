12dad4d13b940dc43eca7d517015b760f677f5dd
Romel <romelps98@gmail.com>
2024-04-12 14:33:03 -0600

    Buzzer with media query and cookie emoji

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const buzzer = document.querySelector(".buzzer");
+const cookie = document.querySelector("#cookie");
+const actualCookie = document.querySelector("#actual-cookie");
+const reveal = document.getElementById("actual-cookie").style.visibility='hidden';
 
+
+actualCookie.visibility = 'hidden';
 buzzer.addEventListener("click", () => {
     cookie.textContent = "You get a cookie!";
-    
+    alert("You got a cookie! ");
+    reveal.visibility = "visible";
 })
 
