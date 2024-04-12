6959f5b2c6b3e40d1c22cfa85e3a39d41f1291ff
Romel <romelps98@gmail.com>
2024-04-12 14:57:02 -0600

    cookie emoji shows up when buzzer is pushed.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const buzzer = document.querySelector(".buzzer");
 const cookie = document.querySelector("#cookie");
 const actualCookie = document.querySelector("#actual-cookie");
-const reveal = document.getElementById("actual-cookie").style.visibility='hidden';
+document.getElementById("actual-cookie").style.visibility='hidden'; //calls actual cookie emoji and hides it
 
 
 actualCookie.visibility = 'hidden';
 buzzer.addEventListener("click", () => {
     cookie.textContent = "You get a cookie!";
     alert("You got a cookie! ");
-    reveal.visibility = "visible";
+    // reveal = "visible";
+    document.getElementById("actual-cookie").style.visibility='visible'; //reveals cookie once buzzer is pushed.
 })
 
