89050bafe73d2142830efd13e3507ba8e635ea5c
Romel <romelps98@gmail.com>
2024-04-14 21:10:31 -0600

    messing with buzzer timing

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const cookie = document.querySelector("#cookie");
 const actualCookie = document.querySelector("#actual-cookie");
 document.getElementById("actual-cookie").style.visibility='hidden'; //calls actual cookie emoji and hides it
 
+// const buzzButton = document.getElementById(".buzzer");
+
 
 actualCookie.visibility = 'hidden';
+buzzer.clicked = false;
+
+
+buzzer.addEventListener("click", () => {
+    cookie.textContent = "You get a cookie!";
+    //alert("You got a cookie! ");
+    // reveal = "visible";
+    document.getElementById("actual-cookie").style.visibility='visible'; //reveals cookie once buzzer is pushed.
+})
 
 const timer = () => {
-    if(buzzer.clicked == true) {
+    if(buzzer.clicked = true) {
         alert('YOU CLICKED THE BUZZER IN TIME! YOU WIN! ... a cookie');
     }
     else {
         alert('YOU LOSE! No cookie.');
     }
+
 }
 
-buzzer.addEventListener("click", () => {
-    cookie.textContent = "You get a cookie!";
-    alert("You got a cookie! ");
-    // reveal = "visible";
-    document.getElementById("actual-cookie").style.visibility='visible'; //reveals cookie once buzzer is pushed.
-})
 
 setTimeout(timer, 5000);
